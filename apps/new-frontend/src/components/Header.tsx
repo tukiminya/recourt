7ba55a6b3df78c2f11dda4e1e3390d8548a3d282
tukiminya <me@tuki.dev>
2026-03-09 20:40:14 +0900

    chore!: use non-default export on Header & add/export HeaderWithFixed, HeaderLogo, HeaderLogoWithCatchCopy component

--- apps/new-frontend/src/components/Header.tsx
+++ apps/new-frontend/src/components/Header.tsx
@@ -1,9 +1,23 @@
-import { LogoWithCatchCopy } from "./Logo";
+import { Logo, LogoWithCatchCopy } from "./Logo";
 
-export default function Header() {
+export function Header({ children }: { children: React.ReactNode }) {
   return (
-    <header className="fixed p-12 top-0 left-1/2 z-50 w-full max-w-500 -translate-x-1/2">
-      <LogoWithCatchCopy className="[&>path]:fill-black mix-blend-difference" />
+    <header className="flex gap-6 items-center justify-between py-16 w-full">{children}</header>
+  );
+}
+
+export function HeaderWithFixed({ children }: { children: React.ReactNode }) {
+  return (
+    <header className="fixed py-12 top-0 left-1/2 z-50 w-full max-w-500 -translate-x-1/2">
+      {children}
     </header>
   );
+}
+
+export function HeaderLogo() {
+  return <Logo className="h-6 [&>path]:fill-black mix-blend-difference" />;
+}
+
+export function HeaderLogoWithCatchCopy() {
+  return <LogoWithCatchCopy className="h-6 [&>path]:fill-black mix-blend-difference" />;
 }
