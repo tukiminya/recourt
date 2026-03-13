37c8ba7b34dbeacb7a7f6d69e6cf8e0e0e2ab21c
tukiminya <me@tuki.dev>
2026-03-14 02:52:40 +0900

    style(new-frontend): update SectionTitle component & remove hr

--- apps/new-frontend/src/components/ui/section.tsx
+++ apps/new-frontend/src/components/ui/section.tsx
@@ -7,8 +7,7 @@ function Section(props: ComponentPropsWithoutRef<"section">) {
 function SectionTitle({ children }: { children: ReactNode }) {
   return (
     <div className="flex gap-4 items-center mb-4">
-      <h2 className="shrink-0 text-neutral-500 text-sm font-sans">{children}</h2>
-      <hr className="border-neutral-100 border flex-1 w-full" />
+      <h2 className="shrink-0 text-neutral-600 text-xs font-sans">{children}</h2>
     </div>
   );
 }
