8219877380c95f0df011a6b5662aafbd49f05d34
tukiminya <me@tuki.dev>
2026-03-14 02:54:05 +0900

    feat(new-frontend): export TanStackLink that wrap recourt Link component with Tanstack Link component

--- apps/new-frontend/src/components/ui/link.tsx
+++ apps/new-frontend/src/components/ui/link.tsx
@@ -1,9 +1,10 @@
+import { type LinkProps, Link as OriginalTanStackLink } from "@tanstack/react-router";
 import { cva, type VariantProps } from "cva";
 import { type LucideProps, LucideSquareArrowOutUpRight } from "lucide-react";
 import type { ComponentPropsWithoutRef } from "react";
 
 const link = cva(
-  "text-blue-800 border-b border-blue-800 visited:text-purple-900 visited:border-purple-900 hover:border-b-2 w-fit inline-flex mx-1 gap-1 items-center",
+  "text-blue-800 underline decoration-1 hover:decoration-2 underline-offset-2 visited:text-purple-900 w-fit inline-flex gap-1 items-center",
   {
     variants: {
       size: {
@@ -26,8 +27,16 @@ function Link(props: ComponentPropsWithoutRef<"a"> & VariantProps<typeof link>) 
   );
 }
 
+function TanStackLink(props: LinkProps & VariantProps<typeof link>) {
+  return (
+    <OriginalTanStackLink className={link({ size: props.size })} {...props}>
+      {props.children}
+    </OriginalTanStackLink>
+  );
+}
+
 function LinkExternalIcon(props: LucideProps) {
   return <LucideSquareArrowOutUpRight {...props} />;
 }
 
-export { Link, LinkExternalIcon };
+export { Link, TanStackLink, LinkExternalIcon };
