da94b9fc185c35a7f149cc0d53e59c3381e72700
tukiminya <me@tuki.dev>
2026-07-04 03:39:26 +0900

    feat: make custom Oxlint plugin for import lucide icon uniformly

---
+++ packages/lint/plugin.ts
@@ -0,0 +1,44 @@
+import { type Rule, eslintCompatPlugin } from "@oxlint/plugins";
+
+const lucideImportRule = {
+  create(context) {
+    return {
+      ImportDeclaration(node) {
+        if (node.source.value === "lucide-react") {
+          node.specifiers.forEach((node) => {
+            if (
+              node.type === "ImportSpecifier" &&
+              node.local.type === "Identifier" &&
+              node.imported.type === "Identifier"
+            ) {
+              if (node.local.name !== node.imported.name) {
+                context.report({
+                  message: "You MUST BE same name between local name & imported name",
+                  node,
+                });
+              }
+              if (!node.local.name.startsWith("Lucide")) {
+                context.report({
+                  message:
+                    "You MUST BE import lucide component, startWith 'Lucide'. For example: `import { LucideX } from 'lucide-react'` ",
+                  node,
+                });
+              }
+            }
+          });
+        }
+      },
+    };
+  },
+} satisfies Rule;
+
+const plugin = eslintCompatPlugin({
+  meta: {
+    name: "recourt-lint",
+  },
+  rules: {
+    "import-lucide-start-by-lucide": lucideImportRule,
+  },
+});
+
+export default plugin;
