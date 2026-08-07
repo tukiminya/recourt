a8e8b5642c2bfb20ec742ccbd9e6d9d401dbcec7
tukiminya <me@tuki.dev>
2026-08-08 02:56:35 +0900

    chore(terraform): remove custom domain setting for workers

--- terraform/workers.tf
+++ terraform/workers.tf
@@ -9,19 +9,7 @@ resource "cloudflare_worker" "recourt-api" {
   }
 }
 
-resource "cloudflare_workers_custom_domain" "recourt-api-custom-domain" {
-  account_id = var.cloudflare_account_id
-  service = cloudflare_worker.recourt-api.name
-  hostname = "api.${var.cloudflare_recourt_domain}"
-}
-
 resource "cloudflare_worker" "recourt-frontend" {
   name = "recourt-frontend"
   account_id = var.cloudflare_account_id
-}
-
-resource "cloudflare_workers_custom_domain" "recourt-frontend-custom-domain" {
-  account_id = var.cloudflare_account_id
-  service = cloudflare_worker.recourt-frontend.name
-  hostname = var.cloudflare_recourt_domain
 }
