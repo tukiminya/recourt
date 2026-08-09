d4fabc2a9cfe49d66db4a2513da1d51557aabdb3
tukiminya <me@tuki.dev>
2026-08-09 23:35:13 +0900

    chore(web): use cloudflare workers for deploy web page & update wrangler to latest version

--- app/web/vite.config.ts
+++ app/web/vite.config.ts
@@ -5,11 +5,11 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { nitro } from "nitro/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), nitro(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [devtools(), cloudflare(), tailwindcss(), tanstackStart(), viteReact()],
 });
 
 export default config;
