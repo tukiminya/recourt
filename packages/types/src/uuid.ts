96229c0da35826f9ff0a9319536d30a02c9c3494
tukiminya <me@tuki.dev>
2026-09-07 18:51:40 +0900

    chore(types): use zod for define UUIDv7 branded type

--- packages/types/src/uuid.ts
+++ packages/types/src/uuid.ts
@@ -1,1 +1,4 @@
-export type UUIDv7 = string & { readonly __brand: "uuidv7" };
+import z from "zod";
+
+export const uuidv7 = z.uuidv7().brand("uuidv7");
+export type UUIDv7 = z.infer<typeof uuidv7>;
