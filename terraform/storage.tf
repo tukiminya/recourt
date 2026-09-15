5a6a8ab1cbb7a326d2222bedb89667cf3b35a58b
tukiminya <me@tuki.dev>
2026-09-15 20:13:09 +0900

    feat: update wrangler compatibility date & add recourt_drafts bucket & add lock for production r2

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -4,6 +4,26 @@ resource "cloudflare_r2_bucket" "recourt" {
   location   = "apac"
 }
 
+resource "cloudflare_r2_bucket" "recourt_drafts" {
+  name       = "recourt-drafts-v1"
+  account_id = var.cloudflare_account_id
+  location   = "apac"
+}
+
+resource "cloudflare_r2_bucket_lock" "recourt_published_articles" {
+  account_id  = var.cloudflare_account_id
+  bucket_name = cloudflare_r2_bucket.recourt.name
+
+  rules = [{
+    id      = "retain-published-articles"
+    enabled = true
+    prefix  = "article/"
+    condition = {
+      type = "Indefinite"
+    }
+  }]
+}
+
 resource "cloudflare_r2_custom_domain" "recourt-custom-domain" {
   account_id  = var.cloudflare_account_id
   bucket_name = cloudflare_r2_bucket.recourt.name
