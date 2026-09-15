28797847c614c8ccb3d04a2226b79f5da2cbabcb
tukiminya <me@tuki.dev>
2026-09-15 20:04:45 +0900

    feat(utils): add ConflictError class

--- packages/utils/src/error.ts
+++ packages/utils/src/error.ts
@@ -1,2 +1,12 @@
 export class NotFoundError extends Error {}
 export class InternalServerError extends Error {}
+
+export class ConflictError extends Error {
+  constructor(
+    message: string,
+    readonly code = "CONFLICT",
+    options?: ErrorOptions,
+  ) {
+    super(message, options);
+  }
+}
